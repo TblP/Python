4c8e139e01ef57920e512f1f61d22a0efcf4d8ad
TblP <vczyplenkov@mail.ru>
2021-05-14 02:17:15 +0300

    zxc

--- lab4/task 1.py
+++ lab4/task 1.py
@@ -10,28 +10,14 @@ if __name__ == "__main__":
 
     deque_.sort()
 
-    print("Sorting integer deque:")
-    for i in range(deque_.size()):
-        print(deque_.pop_back(), end=' ')
-    #Int reverse deque
     deque_ = deque()
-    for i in range(10):
-        deque_.push_back(random.randint(1, 100))
-
-    deque_.sort(lambda lhs, rhs: lhs < rhs)
-
-    print("\nSorting integer deque(reversed):")
-    for i in range(deque_.size()):
-        print(deque_.pop_back(), end=' ')
-
-    #String deque
-    deque_ = deque()
-    with open("text.txt", "r") as file:
+    with open("text.txt", "r", encoding="utf8") as file:
         books_str = file.readlines()
         for i in range(len(books_str)):
             deque_.push_back(books_str[i])
 
     print("\nSorting string deque:")
+
     deque_.sort(lambda lhs, rhs: lhs[0] > rhs[0])
     for i in range(deque_.size()):
-        print(str(i + 1) + "): " + deque_.pop_back(), end='')
+        print(str(i + 1) + "): " + deque_.pop_back() + '\n')
